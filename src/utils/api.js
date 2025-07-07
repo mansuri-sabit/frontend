6f895d39ffc1b76c8232dc18a7116e242ecc6d96
mansuri-sabit <blueflamesabitali2003@gmail.com>
2025-07-07 17:35:56 +0530

    chage apis url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://troikabackend.onrender.com';
+const API_BASE_URL = 'https://openbackend-rvvs.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
